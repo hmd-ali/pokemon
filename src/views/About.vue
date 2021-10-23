09d73c13c370e417f87c48ddad99bca9284c9da8
Ali Hammoud <letsmailali.h@gmail.com>
2021-10-23 22:34:52 +0300

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div v-if="pokemon" class="flex text-white justify-center flex-col items-center max-w-2xl m-auto">
+      <h3 class="text-2xl capitalize text-green-500">{{pokemon.name}}</h3>
+      <div class="flex justify-center items-center">
+        <img v-if="pokemon.sprites.front_shiny" class="w-40" :src="pokemon.sprites.front_shiny" alt="">
+        <img v-if="pokemon.sprites.back_shiny" class="w-40" :src="pokemon.sprites.back_shiny" alt="">
+      </div>
+      <div class="flex justify-center flex-wrap p-4">
+        <h3 class="m-1">Height: {{pokemon.height}}</h3>
+        <h3 class="m-1">Weight: {{pokemon.weight}}</h3>
+      </div>
+      <div class="flex flex-col max-w-md m-auto items-center justify-center">
+        <h3 class="text-xl ">Stats</h3>
+        <div class="flex p-4 flex-wrap max-w-md m-auto items-center justify-center">
+          <div v-for="(stat,idx) in pokemon.stats" :key="idx" class="rounded border-black border-2 m-1.5">
+            <h3 class="capitalize">Base {{stat.stat.name}}: {{stat.base_stat}}</h3>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col items-center justify-center">
+        <h1 class="text-xl">Types</h1>
+        <div class="flex flex-col items-center justify-center">
+          <h3 class="capitalize m-1" v-for="(type,idx) in pokemon.types" :key="idx">{{type.type.name}}</h3>
+        </div>
+      </div>
+    </div>
+    <div v-else class="flex justify-center align-center">
+      <h1 class="m-auto text-2xl text-white">No data available for this pokemon</h1>
+    </div>
   </div>
 </template>
+
+<script>
+
+import { useRoute } from 'vue-router'
+import { reactive, toRefs } from 'vue'
+
+export default {
+  setup(){
+    const route = useRoute()
+    const state = reactive({
+      pokemon: null
+    })
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        state.pokemon = data
+      })
+
+    return {...toRefs(state)}
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full flex justify-center">
+    <input 
+      type="text" 
+      placeholder="Enter Pokemon Name"
+      class="mt-1 p-2 border-2 border-white rounded-lg hover:border-yellow-300 outline-none focus:border-yellow-500 transition-all placeholder-yellow-300"
+      v-model="text"
+     >
+  </div>
+  <div class="mt-10 p-4 flex flex-wrap items-center justify-center">
+    <div
+      class="mt-4 p-4 text-2xl text-blue-500"
+      v-for="(pokemon, idx) in filteredPokemons"
+      :key="idx"
+    >
+      <router-link :to="`/about/${urlIdLookup[pokemon.name]}`">
+        {{ pokemon.name }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import { reactive, toRefs, computed } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup(){
+
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {},
+      text: "",
+      filteredPokemons: computed(() => updatePokemons())
+    })
+
+    const updatePokemons = () => {
+      if(!state.text || state.text.length < 3){
+        return
+      }
+      console.log(state.filteredPokemons)
+      return state.pokemons.filter((pokemon) =>
+        pokemon.name.includes(state.text)
+      )
+    }
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        state.pokemons = data.results
+        state.urlIdLookup = data.results.reduce((acc, curr, idx) => 
+          acc = {...acc,[curr.name]: idx+1}
+        ,{})
+      })
+    
+    return { ...toRefs(state)}
   }
 }
 </script>
